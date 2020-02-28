3ec06183a8d61b40945db4e84a9bcf98d2688ade
ChrissCod <codifrstuff@gmail.com>
2020-02-28 10:40:16 -0500

    adding plots

--- hist.py
+++ hist.py
@@ -1,5 +1,7 @@
 ## This is a code for histograms and stuff
 import numpy as np
+import matplotlib.pyplot as plt
+
 mu = 80
 sigma = 10
 x = np.random.normal(mu, sigma, 1000)
@@ -7,3 +9,8 @@ x = np.random.normal(mu, sigma, 1000)
 print("random normal array mean centered",  x[:10])  
 
 print("mean = ", np.mean(x))
+
+plt.hist(x)
+plt.show()
+
+print("I'm done")
