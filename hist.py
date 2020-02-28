429625bed4c51ca1b0d5fe690567dc86b33c52d4
ChrissCod <codifrstuff@gmail.com>
2020-02-28 10:57:31 -0500

    step back

--- hist.py
+++ hist.py
@@ -1,7 +1,5 @@
 ## This is a code for histograms and stuff
 import numpy as np
-import matplotlib.pyplot as plt
-
 mu = 80
 sigma = 10
 x = np.random.normal(mu, sigma, 1000)
@@ -9,8 +7,3 @@ x = np.random.normal(mu, sigma, 1000)
 print("random normal array mean centered",  x[:10])  
 
 print("mean = ", np.mean(x))
-
-plt.hist(x)
-plt.show()
-
-print("I'm done")
